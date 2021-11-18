a3ea6458181213a3f6f46d84247a8509ff73360a
Sawadaaaaaaaa <17084@hakodate.kosen-ac.jp>
2021-11-18 11:26:14 +0900

    バグ修正

--- game4/MyWorld.java
+++ game4/MyWorld.java
@@ -13,9 +13,25 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1280, 720, 1);
+        setBackground(new GreenfootImage("街.jpg"));
+        
+        this.addObject( new Nakamura(), 100, 360 );
+        this.addObject( new Nakamura_sinzou(), 100, 360 );
+        //this.addObject( new bullet(), 100, 360 );
+        
+        for(int i = 0; i < 5; ++i){
+            int A = 600;
+            int B = 1280;
+            int x = A + (int)(Math.random()*((B-A)+1));
+
+            B = 0;
+            int y = A + (int)(Math.random()*((B-A)+1));
+            addObject( new enemy(), x, y );
+        }
     }
 }
---
+++ game4/Nakamura.java
@@ -0,0 +1,53 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class sawada here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Nakamura extends Actor
+{
+    /**
+     * Act - do whatever the sawada wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private boolean movingleft = false; 
+    public void act() 
+    {
+
+        if( Greenfoot.isKeyDown( "right" ) ){
+            if( movingleft ){
+                setRotation(0);
+                movingleft = false;
+                getImage().mirrorHorizontally();
+            }
+            move(3);
+        }// Add your action code here.
+
+        // Add your action code here.
+        if( Greenfoot.isKeyDown( "left" ) ){
+            if( ! movingleft ){
+                setRotation(0);
+                movingleft = true;
+                getImage().mirrorHorizontally();
+            }
+            move(-3);
+        }
+
+        if( Greenfoot.isKeyDown( "down" ) ){
+            setRotation(45);
+            move(3);
+        }
+        if( Greenfoot.isKeyDown( "up" ) ){
+            setRotation(-45);
+
+            move(3);
+        }
+        if(Greenfoot.isKeyDown("space")){
+
+            getWorld().addObject(new bullet(), getX(), getY());
+
+        }    
+    }
+}
